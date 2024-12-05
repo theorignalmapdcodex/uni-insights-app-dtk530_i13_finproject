3194190e400f5b9ce76610b57fade812e7f19b1e
theoriginalmapdcodex <michaeldaprempeh@gmail.com>
2024-12-04 23:32:53 -0500

    updating current py files

---
+++ gemini_ai_api_key/gemini_api.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+# Loading environment variables from the .env file
+load_dotenv()
+
+# Getting the api key from the environment variable
+the_api_key = os.getenv("DTK530_GEMINI_AI_API_KEY")
+
+# Using the API key in my code
+print(the_api_key)
