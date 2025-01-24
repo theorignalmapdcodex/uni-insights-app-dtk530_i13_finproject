055e0aa2bd05d80fc9f7b9851fc0528c68540ab0
theoriginalmapdcodex <michaeldaprempeh@gmail.com>
2025-01-24 18:44:01 -0500

    deleting env and api file again

--- py_files/gemini_api.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Loading environment variables from the .env file
-load_dotenv()
-
-# Getting the api key from the environment variable and using the API key in my code
-the_api_key = os.getenv("DTK530_I13_GEMINI_AI_API_KEY")
